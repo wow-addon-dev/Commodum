bacd7724e8abd7f4c45d67f821148fdb564b7531
Dionys <72759855+diomsg-code@users.noreply.github.com>
2026-04-28 23:28:37 +0200

    v1.0

--- Commodum.lua
+++ Commodum.lua
@@ -35,29 +35,6 @@ function commodumFrame:ADDON_LOADED(_, addOnName)
 		Utils:InitializeMinimapButton()
 		Options:Initialize()
 
-		local text = ""
-		local _, instanceType, difficulty, difficultyName, _, _, _, instanceID, instanceGroupSize = GetInstanceInfo()
-		local delveInfo, delveInfo2, delveInfo3 = C_UIWidgetManager.GetScenarioHeaderDelvesWidgetVisualizationInfo(6183), C_UIWidgetManager.GetScenarioHeaderDelvesWidgetVisualizationInfo(6184), C_UIWidgetManager.GetScenarioHeaderDelvesWidgetVisualizationInfo(6185)
-		local usedDelveInfo
-		--Nemesis Hack to normal/mythic since tiers aren't numbers
-		if delveInfo2 and delveInfo2.shownState and delveInfo2.shownState == 1 then
-			text = difficultyName .. "(?)"
-		elseif delveInfo3 and delveInfo3.shownState and delveInfo3.shownState == 1 then
-			text = difficultyName .. "(??) "
-		elseif delveInfo and delveInfo.shownState and delveInfo.shownState == 1 then
-			usedDelveInfo = C_UIWidgetManager.GetScenarioHeaderDelvesWidgetVisualizationInfo(6183)
-
-			local delveTier = 0
-			if usedDelveInfo and usedDelveInfo.tierText then
-				---@diagnostic disable-next-line: cast-local-type
-				delveTier = tonumber(usedDelveInfo.tierText)
-			end
-
-			text = difficultyName .. "(" .. delveTier .. ")"
-		end
-
-		Utils:PrintDebug(text)
-
 		Utils:PrintDebug("Addon fully loaded.")
     end
 end
