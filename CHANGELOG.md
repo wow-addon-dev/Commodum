4a712e45d8ff428b6e7912f29602acb7f0f77d1e
Dionys <72759855+diomsg-code@users.noreply.github.com>
2026-08-30 20:16:11 +0200

    v1.21

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,4 @@
 ### [@project-version@](https://github.com/wow-addon-dev/Commodum/tree/@project-version@) (@build-date@)
 [Full Changelog](@full-changelog@) - [Previous Releases](https://github.com/wow-addon-dev/Commodum/releases)
 
-- Changed: General loot notifications can now be hidden separately for items, money, and personal currencies
-- Changed: Quality-of-life options are now organized into expandable sections
-- Changed: Option names were shortened
+- Minor code adjustments
--- data/ChangelogData.lua
+++ data/ChangelogData.lua
@@ -8,6 +8,13 @@ COM.CHANGELOG = {
 		version = version,
 		date = buildDate ~= "" and buildDate or nil,
 		entries = {
+			"Minor code adjustments"
+		}
+	},
+	{
+		version = "v1.20",
+		date = "2026-08-21",
+		entries = {
 			"Changed: General loot notifications can now be hidden separately for items, money, and personal currencies",
 			"Changed: Quality-of-life options are now organized into expandable sections",
 			"Changed: Option names were shortened"
@@ -72,13 +79,6 @@ COM.CHANGELOG = {
 		date = "2026-07-19",
 		entries = {
 			"Added: Automatically sells all copies of items marked per character and, optionally, all poor-quality items when a merchant window opens"
-		}
-	},
-	{
-		version = "v1.11",
-		date = "2026-07-18",
-		entries = {
-			"Minor code adjustments"
 		}
 	}
 }
